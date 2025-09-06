fc1de09651517f244f2e05b843d8f4d561b818b9
Kamma Venkata Siva Rama Krishna <sivaramakrishna456123@gmail.com>
2025-09-06 19:33:18 +0530

    Checking

---
+++ 12.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int num;
+    // cout<<"Enter a number: ";
+    // cin>>num;
+
+    for(int num = 1;num <= 50;num++){
+        if((num%3==0) && (num%5==0)){
+            cout<<num<<" -- FizzBuzz"<<endl;
+        }
+        else if(num%3==0){
+            cout<<num<<" -- Fizz"<<endl;
+        }
+        else if(num%5==0){
+            cout<<num<< " -- Buzz"<<endl;
+        }
+    }
+    
+    
+    return 0;
+}
